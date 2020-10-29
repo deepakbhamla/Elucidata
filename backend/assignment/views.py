62a540d21857c12032fc9af9d56367981a86281d
Deepak Bhamla <dbhamla7@gmail.com>
2020-10-30 00:52:16 +0530

    Bug fix

--- backend/assignment/views.py
+++ backend/assignment/views.py
@@ -35,18 +35,18 @@ class FileUploadView(APIView):
           index_plasmalogen = Acceptence.str.endswith('plasmalogen')
           #   task 1
           data_LPC = data_1.loc[index_LPC]
-          GDRAT_abs_path = os.path.join(MEDIA_ROOT + '/result/LPC.xlsx')
+          GDRAT_abs_path = os.path.join(MEDIA_ROOT + '/LPC.xlsx')
           data_LPC.to_excel(GDRAT_abs_path, index = False, header=True)
           data_PC = data_1.loc[index_PC]
-          GDRAT_abs_path = os.path.join(MEDIA_ROOT + '/result/PC.xlsx')
+          GDRAT_abs_path = os.path.join(MEDIA_ROOT + '/PC.xlsx')
           data_PC.to_excel(GDRAT_abs_path, index = False, header=True)
           data_plasmogen = data_1.loc[index_plasmalogen]
-          GDRAT_abs_path = os.path.join(MEDIA_ROOT + '/result/PLASMA.xlsx')
+          GDRAT_abs_path = os.path.join(MEDIA_ROOT + '/PLASMA.xlsx')
           data_plasmogen.to_excel(GDRAT_abs_path, index = False, header=True)
           #   task 2   
           data_1['Retention Time Roundoff (in mins)']  = data_1.round({'Retention time (min)':0})['Retention time (min)']
           data_1['Retention Time Roundoff (in mins)'].value_counts()
-          GDRAT_abs_path = os.path.join(MEDIA_ROOT + '/result/task2.xlsx')
+          GDRAT_abs_path = os.path.join(MEDIA_ROOT + '/task2.xlsx')
           data_1.to_excel(GDRAT_abs_path, index = False, header=True)
 
           #   task3  
@@ -54,7 +54,7 @@ class FileUploadView(APIView):
           mean_dataframe= task_3_data.groupby(task_3_data['Retention Time Roundoff (in mins)'])
           mean_dataframe = mean_dataframe.mean()
           mean_dataframe = mean_dataframe.reset_index()
-          GDRAT_abs_path = os.path.join(MEDIA_ROOT + '/result/task3.xlsx')
+          GDRAT_abs_path = os.path.join(MEDIA_ROOT + '/task3.xlsx')
           mean_dataframe.to_excel(GDRAT_abs_path, index = False, header=True)
 
           return Response(file_serializer.data, status=status.HTTP_201_CREATED)
@@ -65,7 +65,7 @@ from django.http import HttpResponse
 from wsgiref.util import FileWrapper
 
 def LPCDownloadView(request):
-    file_obj = os.path.join(MEDIA_ROOT + '/result/LPC.xlsx')
+    file_obj = os.path.join(MEDIA_ROOT + '/LPC.xlsx')
     document = open(file_obj, 'rb')   
     response = HttpResponse(FileWrapper(document),content_type='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
     response['Content-Disposition'] = 'attachment; filename="LPC.xlsx"'
@@ -73,7 +73,7 @@ def LPCDownloadView(request):
     return response
 
 def PCDownloadView(request):
-    file_obj = os.path.join(MEDIA_ROOT + '/result/PC.xlsx')
+    file_obj = os.path.join(MEDIA_ROOT + '/PC.xlsx')
     document = open(file_obj, 'rb')   
     response = HttpResponse(FileWrapper(document),content_type='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
     response['Content-Disposition'] = 'attachment; filename="PC.xlsx"'
@@ -82,14 +82,14 @@ def PCDownloadView(request):
 
 
 def PlasmalogenDownloadView(request):
-    file_obj = os.path.join(MEDIA_ROOT + '/result/PLASMA.xlsx')
+    file_obj = os.path.join(MEDIA_ROOT + '/PLASMA.xlsx')
     document = open(file_obj, 'rb')   
     response = HttpResponse(FileWrapper(document),content_type='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
     response['Content-Disposition'] = 'attachment; filename="plasmalogen.xlsx"'
     return response
 
 def RoundDownloadView(request):
-    file_obj = os.path.join(MEDIA_ROOT + '/result/task2.xlsx')
+    file_obj = os.path.join(MEDIA_ROOT + '/task2.xlsx')
     document = open(file_obj, 'rb')   
     response = HttpResponse(FileWrapper(document),content_type='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
     response['Content-Disposition'] = 'attachment; filename="roundoff.xlsx"'
@@ -97,7 +97,7 @@ def RoundDownloadView(request):
 
 
 def MeanDownloadView(request):
-    file_obj = os.path.join(MEDIA_ROOT + '/result/task3.xlsx')
+    file_obj = os.path.join(MEDIA_ROOT + '/task3.xlsx')
     document = open(file_obj, 'rb')   
     response = HttpResponse(FileWrapper(document),content_type='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
     response['Content-Disposition'] = 'attachment; filename="mean.xlsx"'
